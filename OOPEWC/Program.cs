585f014668d4c4acccdfc8cf6c6536ef65e84757
“karandepale” <karandepale111@gmail.com>
2023-05-25 12:19:10 -0700

    Welcome message

---
+++ OOPEWC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPEWC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program using Object oriented programming");
+        }
+    }
+}
