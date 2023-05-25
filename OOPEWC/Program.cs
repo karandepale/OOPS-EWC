7dc57880f2c08d79e3b20a60b5a8c3ef2dce3087
“karandepale” <karandepale111@gmail.com>
2023-05-25 12:29:36 -0700

    Add:Calculate Daily Employee Wage

--- OOPEWC/Program.cs
+++ OOPEWC/Program.cs
@@ -5,16 +5,25 @@ namespace OOPEWC
     internal class Employee
     {
         private bool isPresent;
+        private int wagePerHour;
+        private int workingHours;
 
         public Employee()
         {
             Random random = new Random();
             isPresent = random.Next(0, 2) == 0 ? false : true;
+            wagePerHour = 10; // Assuming the wage per hour is $10
+            workingHours = isPresent ? random.Next(1, 9) : 0; // If present, generate a random working hour between 1 and 8, else 0
         }
 
         public bool IsPresent()
         {
             return isPresent;
+        }
+
+        public int CalculateDailyWage()
+        {
+            return wagePerHour * workingHours;
         }
     }
 
@@ -26,7 +35,8 @@ namespace OOPEWC
 
             if (employee.IsPresent())
             {
-                Console.WriteLine("Employee is Present");
+                int dailyWage = employee.CalculateDailyWage();
+                Console.WriteLine($"Employee is Present. Daily Wage: {dailyWage}");
             }
             else
             {
